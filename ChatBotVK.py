da435232cff94531fb0623f1b05d9b5f6794642b
MargaritaUsova <askacziaa@gmail.com>
2023-12-11 18:17:34 +0300

    подключение к сообществу ВК и начало создания бота

--- ChatBotVK.py
+++ ChatBotVK.py
@@ -1,4 +1,27 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+class Bot:
+    def __int__(self):
+        self.token = token = "vk1.a.atdIug4lHrpR3PGpsny6nalpCoflacYRHzxDErMEWaAtWV8sz9yZd5PumEnlQtI-nceFVBDe1J6ULE0-t50S6vO_ylOspE_ieAuD-LulAsd9iGtwMg_ynwnYmEvgWXFSK3i4DHh4NS7dLQpmxf-E0gF1rZK528Ydv6p32eDMVraG5XK41bLsOAhVREa1bxovxSkQsC21rYhWQgpkVVSeVg"
+        self.vk = vk = vk_api.VkApi(token=self.token)
+        self.longpoll = VkLongPoll(vk)
+
+
+    def write_msg(self, user_id, message):
+        self.vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+
+
+# читаем сообщения от пользователей
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        usersMessage = event.text
+
+        #обработка сообщений пользователя и ответы ему
+        if usersMessage == "привет":
+            write_msg(event.user_id, "Хай")
+        elif usersMessage == "пока":
+            write_msg(event.user_id, "Пока((")
+        else:
+            write_msg(event.user_id, "Не поняла вашего ответа...")
